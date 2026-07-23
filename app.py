a0f1cea6b3690087704bcf3fa5ecb5c21f761005
mittens1124-cell <mittens1124@gmail.com>
2026-07-23 17:27:00 +0900

    Update app.py

--- app.py
+++ app.py
@@ -166,7 +166,7 @@ with tab_sheet1:
                 min_value=0,
                 value=0,
                 step=10000,
-                format="%d",
+                format="%.",
                 key="pre_price",
             )
 
