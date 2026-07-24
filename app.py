4e42b006fcef817b40003fa39afc2d6cea8090b3
Sumin43 <angelakim430@gmail.com>
2026-07-24 18:12:38 +0900

    Update app.py

--- app.py
+++ app.py
@@ -164,7 +164,7 @@ with tab_sheet1:
             selling_price1 = st.number_input(
                 "1인당 판매가 (KRW)",
                 min_value=0.0,
-                value=420000.0,
+                value=0.0,
                 step=10000.0,
                 format="%,d",
                 key="pre_price_v2",
